b5403e5741cc91ac1dcce9dee9dcc424897c2ba1
jhonyarias69 <mjosearboledaguerra@gmail.com>
2024-01-07 17:39:43 -0500

    se agragan librerias multer, se organiza metodo , se crea ruta que recibe un obj odf

--- src/controllers/formulario.controllers.js
+++ src/controllers/formulario.controllers.js
@@ -1,12 +1,44 @@
-import { pool } from '../db.js'
+import { pool } from '../db.js';
+import multer from 'multer';
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 export const RegistrarFormulario = async (peticion, respuesta) => {
+  try {
+    const { nombre, fechaRadicacion, dependencia, asunto } = peticion.body;
+
+    // Accede al archivo adjunto usando req.file
+    const documentoPDF = peticion.file.buffer;
+
+    // Inserta el documento en la base de datos
+    const [result] = await pool.query('INSERT INTO Documentos (nombre, fechaRadicacion, dependencia, asunto, documentoPDF) VALUES (?, ?, ?, ?, ?)', [nombre, fechaRadicacion, dependencia, asunto, documentoPDF]);
+
+    const insertId = result.insertId;
+
+    respuesta.status(201).json({
+      message: 'Documento registrado con éxito',
+      id: insertId,
+      nombre,
+      fechaRadicacion,
+      dependencia,
+      asunto,
+    });
+  } catch (error) {
+    console.error(error);
+    respuesta.status(500).json({ message: '¡Algo salió mal al registrar el documento!' });
+  }
+};
+
+export const uploadMiddleware = upload.single('documentoPDF');
+
+
+/*export const RegistrarFormulario = async (peticion, respuesta) => {
   try {
     const { nombre, fechaRadicacion, dependencia, asunto, documentoPDF } = peticion.body;
 
     // Insertar el documento en la base de datos
-    const [filas] = await pool.query('INSERT INTO Documentos (nombre, fechaRadicacion, dependencia, asunto, documentoPDF) VALUES (?, ?, ?, ?)', [nombre, fechaRadicacion, dependencia, asunto, documentoPDF]);
+    const [filas] = await pool.query('INSERT INTO Documentos (nombre, fechaRadicacion, dependencia, asunto, documentoPDF) VALUES (?, ?, ?, ?, ?)', [nombre, fechaRadicacion, dependencia, asunto, documentoPDF]);
 
     respuesta.send({
       message: 'Documento registrado con éxito',
@@ -21,4 +53,5 @@ export const RegistrarFormulario = async (peticion, respuesta) => {
     console.error(error);
     return respuesta.status(500).json({ message: '¡Algo salió mal al registrar el documento!' });
   }
-};
+};*/
+
--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,9 +1,13 @@
 import {Router} from 'express';
+import multer from 'multer';
 //requerimos las funciones del controlador
 import { ObtenerUsuarios, InsertarUsuarios, IngresarLogin, EliminarUsuario,  } from '../controllers/login.controllers.js';
 
 import { RegistrarFormulario,  } from '../controllers/formulario.controllers.js';
 const router = Router();
+
+const storage = multer.memoryStorage(); // Almacena el archivo en memoria, puedes ajustarlo según tus necesidades
+const upload = multer({ storage: storage });
 
 router.get('/user', ObtenerUsuarios )
 
@@ -13,7 +17,9 @@ router.post('/login', IngresarLogin)
 
 router.delete('/delete/:id', EliminarUsuario)
 
-router.post('/formulario', RegistrarFormulario)
+router.post('/formulario', upload.single('documentoPDF'), RegistrarFormulario);
+
+//router.post('/formulario', RegistrarFormulario)
 
 
 router.options('/user',(req,res)=>{
